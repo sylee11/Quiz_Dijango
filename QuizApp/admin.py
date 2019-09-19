48948cbc33fa4cc379935f717e30a0ab6fa03156
113007-SyLV <113007-SyLV@vbpo.net>
2019-09-19 17:22:27 +0700

    push 19-9

--- QuizApp/admin.py
+++ QuizApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Category, Question
 # Register your models here.
+
+#admin.site.register(Category)
+#admin.site.register(Question)
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ['id', 'name']
+admin.site.register(Category, CategoryAdmin)
+
+class QuestionAdmin(admin.ModelAdmin):
+	list_display = ['id', 'question' , 'anser', 'category_id']
+	search_fields = ['question']
+admin.site.register(Question, QuestionAdmin)
--- QuizApp/models.py
+++ QuizApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length = 100)
+	def __str__(self):
+		return(self.name)
+		
+class Question(models.Model):
+	question = models.CharField(max_length =40, blank = True, null = True)
+	anser1 = models.CharField(max_length= 50, null = True)
+	anser2 = models.CharField(max_length= 50, null = True)
+	anser3 = models.CharField(max_length= 50, null = True)
+	anser4 = models.CharField(max_length= 50, null = True)
+	anser = models.IntegerField()
+	user_id = models.ForeignKey(User, on_delete = models.CASCADE)
+	category_id = models.ForeignKey(Category, on_delete = models.CASCADE)
+	def __str__(self):
+		return(self.question)
